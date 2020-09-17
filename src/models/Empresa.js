7b3d4d2655f992ac5a0f3c226b0c3c513662db5d
Maria Eduarda Kovalski <eduardakovalski@gmail.com>
2020-09-17 10:10:42 -0300

    descomentar

--- src/models/Empresa.js
+++ src/models/Empresa.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose");
 const PointSchema = require("./utils/PointSchema");
 
 const SchemaEmpresa = new mongoose.Schema({
-  // foto: String,
+  foto: String,
   empresa: String,
-  // cnpj: Number,
-  // contato: Number,
-  // redeSocial: String,
+  cnpj: Number,
+  contato: String,
+  redeSocial: String,
   email: String,
   endereco: [String],
   localizacao: {
     type: PointSchema,
     index: "2dsphere",
   },
-  // zonasAtuacao: String,
-  // faixaPreco: [Number],
-  // vans: [String],
-  // onibus: [String],
+  zonasAtuacao: String,
+  faixaPreco: [Number],
+  vans: [String],
+  onibus: [String],
 });
 
 module.exports = mongoose.model("Empresa", SchemaEmpresa);
